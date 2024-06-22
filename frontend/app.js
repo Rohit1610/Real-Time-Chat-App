61a86266e8a0b6d3c62c89ae9a8f69e13d78542d
rohit <rohitgpatil16@gmail.com>
2024-06-22 23:51:13 +0530

    socket.io

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,10 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = new io("ws://localhost:3500");
 
 function sendMessage(e) {
   e.preventDefault();
   const input = document.querySelector("input");
   if (input.value) {
-    socket.send(input.value);
+    socket.emit("message", input.value);
     input.value = "";
   }
   input.focus();
@@ -13,7 +13,7 @@ function sendMessage(e) {
 document.querySelector("form").addEventListener("submit", sendMessage);
 
 // Listen for messages
-socket.addEventListener("message", ({ data }) => {
+socket.on("message", (data) => {
   const li = document.createElement("li");
   li.textContent = data;
   document.querySelector("ul").appendChild(li);
--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
-const ws = require("ws");
-// instance of ws
-const server = new ws.Server({ port: "3000" });
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-server.on("connection", (socket) => {
-  socket.on("message", (message) => {
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} Connected`);
+  socket.on("message", (data) => {
     // buffer for binary data
-    const b = Buffer.from(message);
-    console.log(b.toString());
-    socket.send(`${message}`);
+    // const b = Buffer.from(message);
+    console.log(data);
+    io.emit(`message`, `${socket.id.substring(0, 5)}${data}`);
   });
 });
+
+httpServer.listen(3500, () => console.log("listening on port 3500"));
